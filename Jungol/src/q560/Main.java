723f04b2823fd2d899f5631d9d1dd3f760a39af3
manic-066g <xeungjin@nate.com>
2024-04-03 17:34:01 +0900

    560, 572

--- Jungol/src/q560/Main.java
+++ Jungol/src/q560/Main.java
@@ -1,5 +1,6 @@
 package q560;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -11,18 +12,29 @@ public class Main {
 		}
 		sc.close();
 
+		//print
 		for (int i = 0; i < ar.length; i++) {
 			System.out.print(ar[i] + " ");
 		}
-
-		int min = 1000;
-//		min = Integer.MAX_VALUE;
-
+		System.out.println();
+		
+		//2nd case
+		Arrays.sort(ar);
+		
 		for (int i = 0; i < ar.length; i++) {
-			if(min > ar[i]) {
-				min = ar[i];
-			}
+			System.out.print(ar[i] + " ");
 		}
-		System.out.println(min);
+		
+		
+		//1st case
+//		int min = 1000;
+////		min = Integer.MAX_VALUE;
+//
+//		for (int i = 0; i < ar.length; i++) {
+//			if(min > ar[i]) {
+//				min = ar[i];
+//			}
+//		}
+//		System.out.println(min);
 	}
 }
